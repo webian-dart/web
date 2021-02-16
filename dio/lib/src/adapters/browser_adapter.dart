86cc7986cc0258dfc23ffabb875b2e682643322f
Carlos Marques <carlos.marques@mkodo.com>
2021-02-16 18:41:04 +0000

    Shrunk BrowserAdapter Fetch and broke it down in parts

--- dio/lib/src/adapters/browser_adapter.dart
+++ dio/lib/src/adapters/browser_adapter.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
-import 'dart:typed_data';
-import '../dio_error.dart';
-import '../options.dart';
-import '../adapter.dart';
 import 'dart:html';
+import 'dart:typed_data';
+
+import '../adapter.dart';
+import '../dio_error.dart';
 import '../headers.dart';
+import '../options.dart';
 
 HttpClientAdapter createAdapter() => BrowserHttpClientAdapter();
 
@@ -23,73 +24,41 @@ class BrowserHttpClientAdapter implements HttpClientAdapter {
   @override
   Future<ResponseBody> fetch(RequestOptions options,
       Stream<List<int>>? requestStream, Future? cancelFuture) {
-    var xhr = HttpRequest();
+    final xhr = HttpRequest();
+    final completer = Completer<ResponseBody>();
     _xhrs.add(xhr);
+    _setupRequest(xhr, options);
+    _setupHandlers(xhr,
+        completer: completer, options: options, cancelFuture: cancelFuture);
+    _start(xhr, requestStream);
+    return completer.future.whenComplete(() => _xhrs.remove(xhr));
+  }
 
+  void _setupHandlers(HttpRequest xhr,
+      {required Completer<ResponseBody> completer,
+      required RequestOptions options,
+      required Future? cancelFuture}) {
+    xhr.onLoad.first
+        .then((_) => _onResponse(xhr, completer: completer, options: options));
+
+    xhr.onError.first.then((_) {
+      // Unfortunately, the underlying XMLHttpRequest API doesn't expose any
+      // specific information about the error itself.
+      _onError('XMLHttpRequest error.', options: options, completer: completer);
+    });
+    cancelFuture?.then((_) => _cancelFuture(xhr));
+  }
+
+  void _setupRequest(HttpRequest xhr, RequestOptions options) {
     xhr
       ..open(options.method!, options.uri.toString(), async: true)
       ..responseType = 'blob'
       ..withCredentials = options.extra['withCredentials'] ?? withCredentials;
     options.headers.remove(Headers.contentLengthHeader);
     options.headers.forEach((key, v) => xhr.setRequestHeader(key, '$v'));
+  }
 
-    var completer = Completer<ResponseBody>();
-
-    xhr.onLoad.first.then((_) {
-      // TODO: Set the response type to "arraybuffer" when issue 18542 is fixed.
-      var blob = xhr.response ?? Blob([]);
-      var reader = FileReader();
-
-      reader.onLoad.first.then((_) {
-        var body = reader.result as Uint8List;
-        completer.complete(
-          ResponseBody.fromBytes(
-            body,
-            xhr.status,
-            headers:
-                xhr.responseHeaders.map((k, v) => MapEntry(k, v.split(','))),
-            statusMessage: xhr.statusText,
-            isRedirect: xhr.status == 302 || xhr.status == 301,
-          ),
-        );
-      });
-
-      reader.onError.first.then((error) {
-        completer.completeError(
-          DioError(
-            type: DioErrorType.RESPONSE,
-            error: error,
-            request: options,
-          ),
-          StackTrace.current,
-        );
-      });
-      reader.readAsArrayBuffer(blob);
-    });
-
-    xhr.onError.first.then((_) {
-      // Unfortunately, the underlying XMLHttpRequest API doesn't expose any
-      // specific information about the error itself.
-      completer.completeError(
-        DioError(
-          type: DioErrorType.RESPONSE,
-          error: 'XMLHttpRequest error.',
-          request: options,
-        ),
-        StackTrace.current,
-      );
-    });
-
-    cancelFuture?.then((_) {
-      if (xhr.readyState < 4 && xhr.readyState > 0) {
-        try {
-          xhr.abort();
-        } catch (e) {
-          // ignore
-        }
-      }
-    });
-
+  void _start(HttpRequest xhr, Stream<List<int>>? requestStream) {
     if (requestStream == null) {
       xhr.send();
     } else {
@@ -97,10 +66,41 @@ class BrowserHttpClientAdapter implements HttpClientAdapter {
           .reduce((a, b) => Uint8List.fromList([...a, ...b]))
           .then(xhr.send);
     }
+  }
 
-    return completer.future.whenComplete(() {
-      _xhrs.remove(xhr);
+  void _cancelFuture(HttpRequest xhr) {
+    if (xhr.readyState < 4 && xhr.readyState > 0) {
+      try {
+        xhr.abort();
+      } catch (e) {
+        // ignore
+      }
+    }
+  }
+
+  void _onResponse(HttpRequest xhr,
+      {required Completer<ResponseBody> completer,
+      required RequestOptions options}) {
+    // TODO: Set the response type to "arraybuffer" when issue 18542 is fixed.
+    var blob = xhr.response ?? Blob([]);
+    var reader = FileReader();
+
+    reader.onLoad.first.then((_) {
+      var body = reader.result as Uint8List;
+      completer.complete(
+        ResponseBody.fromBytes(
+          body,
+          xhr.status,
+          headers: xhr.responseHeaders.map((k, v) => MapEntry(k, v.split(','))),
+          statusMessage: xhr.statusText,
+          isRedirect: xhr.status == 302 || xhr.status == 301,
+        ),
+      );
     });
+
+    reader.onError.first.then(
+        (error) => _onError(error, options: options, completer: completer));
+    reader.readAsArrayBuffer(blob);
   }
 
   /// Closes the client.
@@ -114,5 +114,18 @@ class BrowserHttpClientAdapter implements HttpClientAdapter {
       }
     }
     _xhrs.clear();
+  }
+
+  void _onError(dynamic error,
+      {required Completer<ResponseBody> completer,
+      required RequestOptions options}) {
+    completer.completeError(
+      DioError(
+        type: DioErrorType.RESPONSE,
+        error: error,
+        request: options,
+      ),
+      StackTrace.current,
+    );
   }
 }
